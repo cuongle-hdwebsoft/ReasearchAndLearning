812d27670d63b62c52f4755cd7d93b78116b5f01
Ram4GB <leminhcuong2988@yahoo.com>
2022-04-26 21:08:57 +0700

    init shop source

--- VueJS/vue3-todos/src/main.js
+++ VueJS/vue3-todos/src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import Page1Component from "./pages/Page1Component";
-import Page2Component from "./pages/Page2Component";
+import HomePage from "./pages/HomePage";
 import * as VueRouter from "vue-router";
-import firstPlugin from "./plugins/FirstPlugin";
 
 const routes = [
   {
-    path: "/page1",
-    component: Page1Component,
-  },
-  {
-    path: "/page2",
-    component: Page2Component,
+    path: "/",
+    component: HomePage,
   },
 ];
 
@@ -23,5 +17,4 @@ const router = VueRouter.createRouter({
 
 const app = createApp(App);
 app.use(router);
-app.use(firstPlugin);
 app.mount("#app");
--- VueJS/vue3-todos/src/pages/Page2Component.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>Page 2</div>
-</template>
-
-<script>
-export default {
-  name: "Page2Component",
-};
-</script>
