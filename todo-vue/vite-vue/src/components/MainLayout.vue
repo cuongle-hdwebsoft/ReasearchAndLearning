dfcd829e01594f2049acb340484e899bfeb89280
Ram4GB <leminhcuong2988@yahoo.com>
2022-05-15 21:08:18 +0700

    init layout

--- todo-vue/vite-vue/src/components/MainLayout.vue
+++ todo-vue/vite-vue/src/components/MainLayout.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="wrapper-root">
     <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>News</li>
-          <li>Login</li>
+      <nav class="navbar">
+        <ul class="navbar__nav">
+          <li class="navbar__item">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="navbar__item">
+            <router-link to="/news">News</router-link>
+          </li>
+          <li class="navbar__item">
+            <router-link to="/login">Login</router-link>
+          </li>
         </ul>
       </nav>
     </header>
@@ -21,5 +27,63 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+  font-family: "Source Code Pro", monospace;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+header {
+  margin: 0;
+}
+
+.wrapper-root {
+  margin: 0;
+  padding: 0;
+}
+
+.navbar {
+  padding: 0;
+  margin: 0;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+  &__nav {
+    display: flex;
+    list-style: none;
+    padding: 12px;
+    margin: 2px;
+  }
+
+  &__item {
+    padding: 10px;
+    cursor: pointer;
+
+    & a {
+      color: #111;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+        font-weight: bold;
+      }
+    }
+
+    &:last-child {
+      margin-left: auto;
+    }
+  }
+}
+
+.container {
+  width: 80%;
+  margin: auto;
+}
 </style>
---
+++ todo-vue/vite-vue/src/pages/news.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>News page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
