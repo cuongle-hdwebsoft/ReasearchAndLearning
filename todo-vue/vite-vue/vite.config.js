404643d9ac373a32fda1622c2252c0cab63ae41b
Ram4GB <leminhcuong2988@yahoo.com>
2022-05-16 13:09:51 +0700

    add .evn.dev

--- todo-vue/vite-vue/vite.config.js
+++ todo-vue/vite-vue/vite.config.js
@@ -1,39 +1,50 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import Inspect from "vite-plugin-inspect";
 
-export default defineConfig({
-  publicDir: "public",
-  cacheDir: ".vite",
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "src/assets/scss/global.scss";',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_URL,
+    mode: env.VITE_MODE,
+    publicDir: "public",
+    cacheDir: ".vite",
+    server: {
+      host: env.VITE_HOST,
+      port: env.VITE_PORT,
+      strictPort: true,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/assets/scss/global.scss";',
+        },
       },
     },
-  },
-  plugins: [
-    Vue({
-      include: [/\.vue$/],
-    }),
-    Pages({
-      dirs: "src/pages",
-      resolver: "vue",
-      extensions: ["vue"],
-      moduleId: "~custom-route-pages",
-    }),
-    AutoImport({
-      imports: [
-        "vue",
-        {
-          "vue-router": ["createRouter", "createWebHistory"],
-        },
-      ],
-    }),
-    Components(),
-    Inspect(),
-  ],
+    plugins: [
+      Vue({
+        include: [/\.vue$/],
+      }),
+      Pages({
+        dirs: "src/pages",
+        resolver: "vue",
+        extensions: ["vue"],
+        moduleId: "~custom-route-pages",
+      }),
+      AutoImport({
+        imports: [
+          "vue",
+          {
+            "vue-router": ["createRouter", "createWebHistory"],
+          },
+        ],
+      }),
+      Components(),
+      Inspect(),
+    ],
+  };
 });
