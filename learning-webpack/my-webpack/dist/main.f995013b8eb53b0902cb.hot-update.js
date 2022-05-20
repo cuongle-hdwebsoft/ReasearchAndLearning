c74b2aec1553b084634967b68ce3d260d507ff78
Ram4GB <leminhcuong2988@yahoo.com>
2022-05-20 12:57:15 +0700

    babel commonjs

---
+++ learning-webpack/my-webpack/dist/main.f995013b8eb53b0902cb.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatemy_webpack"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\src\\\\index.js: @babel/preset-env: The 'modules' option must be one of \\n - 'false' to indicate no module processing\\n - a specific module type: 'commonjs', 'amd', 'umd', 'systemjs' - 'auto' (default) which will automatically select 'false' if the current\\n   process is known to support ES module syntax, or \\\"commonjs\\\" otherwise\\n (While processing: \\\"C:\\\\\\\\Users\\\\\\\\lemin\\\\\\\\Desktop\\\\\\\\HDWEBSOFT\\\\\\\\learning-webpack\\\\\\\\my-webpack\\\\\\\\node_modules\\\\\\\\@babel\\\\\\\\preset-env\\\\\\\\lib\\\\\\\\index.js\\\")\\n    at OptionValidator.invariant (C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\helper-validator-option\\\\lib\\\\validator.js:48:13)\\n    at validateModulesOption (C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\normalize-options.js:78:5)\\n    at normalizeOptions (C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\normalize-options.js:141:14)\\n    at C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js:272:37\\n    at C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\helper-plugin-utils\\\\lib\\\\index.js:23:12\\n    at async (C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:44:33)\\n    at async (C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\gensync\\\\index.js:186:15)\\n    at C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\gensync\\\\index.js:216:13\\n    at Generator.next (<anonymous>)\\n    at C:\\\\Users\\\\lemin\\\\Desktop\\\\HDWEBSOFT\\\\learning-webpack\\\\my-webpack\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:229:21\");\n\n//# sourceURL=webpack://my-webpack/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("f3ae241ec9fe378f4b2c")
+/******/ })();
+/******/ 
+/******/ }
+);
